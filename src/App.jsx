a5499407fa570f75e2a76c1d304ccddbfc6e9295
Alan <lic.sanchezalan@gmail.com>
2024-10-28 09:12:46 -0300

    unnecessary modules update

--- src/App.jsx
+++ src/App.jsx
@@ -12,6 +12,7 @@ import './styles/NotFound.css'
 import Header from './components/Header';
 import Footer from './components/Footer';
 
+
 function App( { children } ) {
 
   return (
--- src/main.jsx
+++ src/main.jsx
@@ -34,7 +34,6 @@ const router = createBrowserRouter([
     element: <Contact />
   },
 ]);
-
 ReactDOM.createRoot(document.getElementById('root')).render(
   <StrictMode>
     <RouterProvider router={router} />
