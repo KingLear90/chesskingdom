d6bb8443d0f5e00df54bec354617f1a447b16c6a
Alan <lic.sanchezalan@gmail.com>
2024-10-31 11:44:45 -0300

    comments on Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -12,7 +12,7 @@ function Header( {brandName, brandURL, navLinks} ) {
                 <div className="collapse navbar-collapse" id="navbarSupportedContent">
                     <ul className="navbar-nav ml-auto">
                         { navLinks.map((link, index) => (
-                            <li className="navbar-item" key={index}>   { /* Como navLinks es un array, utilizamos una key para identificar cada li, que será el elemento único que se repite.*/}
+                            <li className="navbar-item" key={index}>   { /* Como navLinks es un array, genero una key para identificar cada li, que será el elemento único que se repite.*/}
                                 <a href= {link.url} className="nav-link">{link.name}</a>
                             </li>
                         ))}
@@ -24,7 +24,7 @@ function Header( {brandName, brandURL, navLinks} ) {
 
 }
 
-Header.propTypes = {
+Header.propTypes = {    // Se definen las props que se espera recibir en cada propiedad.
     brandName: PropTypes.string.isRequired,
     brandURL: PropTypes.string.isRequired,
     navLinks: PropTypes.array.isRequired,
