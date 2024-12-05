f96688d053219c20fa544447fd1a28c9ec77b617
Alan <lic.sanchezalan@gmail.com>
2024-12-05 14:45:46 -0300

    footer style update

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -14,13 +14,16 @@ export default function Footer({ title, terminos, link1, img1, link2, img2, pare
     return (
         <footer className="foot">
           <div className='footer-info'>
-            <p>{title}</p><p> <a href={terminos} className='rights'>Privacy Policy</a></p>
+            <p>{title}</p>
+          </div>
+          <div className='footer-links'>
+            <button className='contact' onClick={parentMethod}>Contacto</button>
+            <p><a href={terminos} className='rights'>Privacy Policy</a></p>
           </div>
           <div className='footer-icons'>
             <a href={link1}>{img1}</a> 
             <a href={link2}>{img2}</a>
           </div>
-          <div><button className='contact' onClick={parentMethod}>CONTACTO</button></div>
         </footer>
     )
 }
