b389a75c72b94b03d42dd486c91e2d8621e92a7f
Alan S. <lic.sanchezalan@gmail.com>
2024-12-28 13:26:48 -0300

    footer items update

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -2,15 +2,12 @@ import './Footer.css'
 
 interface FooterProps {
     title: string;
-    terminos: string;
     link1: string;
     img1: string | JSX.Element;
-    link2: string;
-    img2: string | JSX.Element;
     parentMethod: () => void;
 }
 
-export default function Footer({ title, terminos, link1, img1, link2, img2, parentMethod }: FooterProps) {
+export default function Footer({ title, link1, img1, parentMethod }: FooterProps) {
     return (
         <footer className="foot">
           <div className='footer-info'>
@@ -18,11 +15,9 @@ export default function Footer({ title, terminos, link1, img1, link2, img2, pare
           </div>
           <div className='footer-links'>
             <button className='contact' onClick={parentMethod}>Contacto</button>
-            <p><a href={terminos} className='rights'>Privacy Policy</a></p>
           </div>
           <div className='footer-icons'>
             <a href={link1}>{img1}</a> 
-            <a href={link2}>{img2}</a>
           </div>
         </footer>
     )
