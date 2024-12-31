7f02ffe9c5ee8b32435d38f95e6dca01b0060f2c
Alan S. <lic.sanchezalan@gmail.com>
2024-12-31 19:43:40 -0300

    style changes (login)

--- src/Pages/SignIn/SignIn.tsx
+++ src/Pages/SignIn/SignIn.tsx
@@ -1,17 +1,26 @@
-import { useNavigate } from 'react-router-dom';
-import App from '../../App';
 import './SignIn.css';
+import App from '../../App';
+import { useNavigate } from 'react-router-dom';
 import { useState } from 'react';
 
 function SignIn() {
-  
+  const [sesion, setSesion] = useState(false)
+  const [form, setForm] = useState({
+    email: '',
+    password: ''
+  })
+
   const navigate = useNavigate(); 
 
   const toSignUp = () => {
    navigate('/signup')
   }
 
-  const [sesion, setSesion] = useState(false)
+  fetch("http://localhost:3000/api/user/get")
+  .then(res => res.json())
+  .then(data => console.log(data)) 
+
+ 
 
   const handleSubmit = (event: any) => {
     event.preventDefault();         
@@ -21,6 +30,7 @@ function SignIn() {
     }, 2000);                        // El mensaje dura 2 segundos y desaparece. 
   }
 
+
   
   return (
     <App>
@@ -28,20 +38,23 @@ function SignIn() {
             <h4 className="formTitle">Iniciar sesión:</h4>
 
             <label htmlFor="email">Usuario (correo electrónico):</label>
-            <input type="email" name="email" /> 
+            <input type="email" name="email" required 
+            value={form.email} onChange={e => setForm(prevForm => ({ ...prevForm, email: e.target.value }))} />   
 
             <label htmlFor="password">Contraseña:</label>
-            <input type="password" name="password" /> 
+            <input type="password" name="password" required 
+            value={form.password} onChange={e=> setForm(prevForm => ({ ...prevForm, password: e.target.value }))} /> 
 
             <div className="formButtonContainer">
-              <button type="submit" className="btn btn-primary">ACCEDER</button>
+              <button type="submit" className="btn btn-success py-2">ACCEDER</button>
             </div>
             {/* Como REGISTRO es true, se muestra el mensaje de confirmación */}
             {sesion && <h4 className='thanksMsg'>¡Disfruta el sitio!</h4>} 
-        </form>
-        <div>
-            <button className="btn btn-secondary" onClick={toSignUp}>Registrarse (GRATIS)</button>
+            <div>
+            <button className="btn btn-primary py-2" onClick={toSignUp}>REGISTRARSE (GRATIS)</button>
         </div>
+        </form>
+        
     </App>
   )
 }
